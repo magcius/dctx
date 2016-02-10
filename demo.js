c2a42eed711cf757de7dbb6bc032f8d003f25cd5
Jasper St. Pierre <jstpierre@mecheye.net>
2016-02-09 22:17:27 -0800

    get it working

--- demo.js
+++ demo.js
@@ -11,18 +11,20 @@
         };
     }
 
+    var TAU = Math.PI * 2;
+
     function dct(pixels, coeffs) {
         var N = 8;
+        var tmp = new Float32Array(8);
 
-        function dct_1d(offs, stride) {
+        function dct_1d(dst, src, offset, stride) {
             for (var i = 0; i < N; i++) {
                 var sum = 0;
-                var frequency = TAU * (1/4) * k;
 
                 for (var j = 0; j < N; j++) {
                     var theta = (2*j+1) / N;
-                    var pix = pixels[offset + j * stride];
-                    sum += pixel * Math.cos(frequency * theta);
+                    var pixel = src[offset + j * stride];
+                    sum += pixel * Math.cos(Math.PI * i * (2*j+1) / (2*N));
                 }
 
                 if (i == 0)
@@ -30,16 +32,25 @@
                 else
                     sum /= 2;
 
-                coeffs[offset + i * stride] = sum;
+                tmp[i] = sum;
             }
+
+            for (var i = 0; i < N; i++)
+                dst[offset + i * stride] = tmp[i];
         }
 
-        var x, y;
-        for (y = 0; y < N; y++) {
-        }
+        var i;
+
+        // rows
+        for (i = 0; i < N; i++)
+            dct_1d(coeffs, pixels, N * i, 1);
+
+        // columns
+        for (i = 0; i < N; i++)
+            dct_1d(coeffs, coeffs, i, N);
     }
 
-    function idct(pixels, coeffs) {
+    function idct(pixels, coeffs, divisor) {
         var x, y;
         var N = 8;
 
@@ -69,6 +80,7 @@
                         var harmonicY = Math.cos((2*y+1) / N * freqY);
 
                         var coef = coeffs[ky*N+kx];
+                        coef = coef / divisor;
                         var weightedHarmonic = coef * harmonicX * harmonicY;
 
                         // XXX -- figure out what this is here for; seems arbitrary,
@@ -97,12 +109,12 @@
         for (var by = 0; by < imgData.height; by += BLOCK_SIZE) {
             for (var bx = 0; bx < imgData.width; bx += BLOCK_SIZE) {
                 var block = new Uint8Array(BLOCK_SIZE * BLOCK_SIZE);
-                var bOff = by*imgData.width+bx;
+                var imgBoff = by*imgData.width+bx;
                 for (var y = 0; y < BLOCK_SIZE; y++) {
                     for (var x = 0; x < BLOCK_SIZE; x++) {
-                        var off = y*BLOCK_SIZE+x;
-                        block[off] = imgData.data[bOff+off];
-                        console.log(bOff, off, imgData.data);
+                        var blockOff = y*BLOCK_SIZE+x;
+                        var imgOff = y*imgData.width+x;
+                        block[blockOff] = imgData.data[(imgBoff+imgOff)*4];
                     }
                 }
                 blocks.push(block);
@@ -110,20 +122,24 @@
         }
         return blocks;
     }
-    function blocksToImgData(imgData, blocks) {
+    function blocksToImgData(imgData, blocks, divisor) {
+        if (!divisor) divisor = 1;
         var nBlocksPerRow = (imgData.width / BLOCK_SIZE) | 0;
         blocks.forEach(function(block, i) {
-            var block = new Uint8Array(BLOCK_SIZE * BLOCK_SIZE);
-            var by = (imgData.width / nBlocksPerRow) | 0;
-            var bx = (imgData.width % nBlocksPerRow) | 0;
-            var bOff = by*BLOCK_SIZE+bx;
+            var by = ((i / nBlocksPerRow) | 0) * BLOCK_SIZE;
+            var bx = ((i % nBlocksPerRow) | 0) * BLOCK_SIZE;
+
+            var imgBoff = by*imgData.width+bx;
             for (var y = 0; y < BLOCK_SIZE; y++) {
                 for (var x = 0; x < BLOCK_SIZE; x++) {
-                    var off = y*BLOCK_SIZE+x;
-                    imgData.data[bOff+off] = block[off];
+                    var blockOff = y*BLOCK_SIZE+x;
+                    var imgOff = y*imgData.width+x;
+                    imgData.data[(imgBoff+imgOff)*4+0] = block[blockOff] / divisor;
+                    imgData.data[(imgBoff+imgOff)*4+1] = block[blockOff] / divisor;
+                    imgData.data[(imgBoff+imgOff)*4+2] = block[blockOff] / divisor;
+                    imgData.data[(imgBoff+imgOff)*4+3] = 0xFF;
                 }
             }
-            blocks.push(block);
         });
     }
 
@@ -136,13 +152,48 @@
         ctx.drawImage(img, 0, 0);
 
         var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        var blocks = blocksFromImgData(imgData);
+        var pixelBlocks = blocksFromImgData(imgData);
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        var coeffBlocks = [];
+        pixelBlocks.forEach(function(pixels) {
+            var coeffs = new Float32Array(pixels.length);
+            dct(pixels, coeffs);
+            coeffBlocks.push(coeffs);
+        });
+
+        var currDivisor = 1;
+        var showCoeffs = false;
+
+        function redisplay() {
+            if (showCoeffs) {
+                blocksToImgData(imgData, coeffBlocks, currDivisor);
+            } else {
+                for (var i = 0; i < pixelBlocks.length; i++) {
+                    idct(pixelBlocks[i], coeffBlocks[i], currDivisor);
+                }
+                blocksToImgData(imgData, pixelBlocks);
+            }
+            ctx.putImageData(imgData, 0, 0);
+        }
+
         imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        blocksToImgData(imgData, blocks);
-        ctx.putImageData(imgData, 0, 0);
+
+        var slider = document.querySelector('#divisorinput');
+        slider.value = currDivisor;
+        slider.oninput = function() {
+            document.querySelector('#divisordisp').textContent = slider.value;
+        };
+        slider.onchange = function() {
+            currDivisor = slider.value;
+            redisplay();
+        };
+        document.querySelector('#showcoeffs').onchange = function() {
+            showCoeffs = this.checked;
+            redisplay();
+        };
+        redisplay();
     }
 
     window.onload = function() {
